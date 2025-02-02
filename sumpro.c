be9b91367775ec180740ab494c65e85f271d56f8
Arseny M <mail@arsm.me>
2025-02-02 22:50:10 +0300

    Handle single digit input

--- sumpro.c
+++ sumpro.c
@@ -1,6 +1,23 @@
+#include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
-int main() {
-  printf("hello again...\n");
+const char *single_digit[]= { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    return 1;
+  }
+
+  char *input = argv[1];
+  int input_length = strlen(input);
+
+  if (input_length > 1) {
+    return 1;
+  }
+
+  // TODO: Handle invalid conversions
+  const int digit = atoi(input);
+  printf("%s\n", single_digit[digit]);
   return 0;
 }
